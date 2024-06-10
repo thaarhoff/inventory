5349d0e355ef96af82d952ff7b9862bb6b1d6abc
Thomas Haarhoff <thomas.haarhoff@hypertegrity.de>
2024-06-10 10:18:14 +0200

    changed output file to yaml

--- generator/main.js
+++ generator/main.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
-    $('#configForm').on('submit', function(event) {
-      event.preventDefault();
-  
-      const configName = $('#configName').val();
-      const configOption = $('#configOption').val();
-  
-      const configData = {
-        name: configName,
-        option: configOption
-      };
-  
-      const blob = new Blob([JSON.stringify(configData, null, 2)], { type: 'application/json' });
-      saveAs(blob, 'config.json');
-    });
+  $('#configForm').on('submit', function(event) {
+    event.preventDefault();
+
+    const configName = $('#configName').val();
+    const configOption = $('#configOption').val();
+
+    const configData = {
+      name: configName,
+      option: configOption
+    };
+
+    const yamlStr = jsyaml.dump(configData);
+    const blob = new Blob([yamlStr], { type: 'application/x-yaml' });
+    saveAs(blob, 'config.yaml');
   });
+});
